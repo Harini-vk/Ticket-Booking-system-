e6a94bef5609c84b975d04724f5b9580a5216504
Harini <vharini1505@gmail.com>
2025-09-20 17:14:44 +0530

    updated

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -3,12 +3,10 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 const firebaseConfig = {
-  apiKey: "AIzaSyCEr5rg4nMZM_0JorAm1Y9JWm4-4kpF76o",
-  authDomain: "ticketsystem-56bcd.firebaseapp.com",
-  projectId: "ticketsystem-56bcd",
-  storageBucket: "ticketsystem-56bcd.firebasestorage.app",
-  messagingSenderId: "962259990909",
-  appId: "1:962259990909:web:45c2c693bf96eb3183f414"
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_AUTH_DOMAIN",
+  projectId: "YOUR_PROJECT_ID",
+  // ... other config
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
